8cad23cac84e420f9da02248829bbfcc93ac9e7f
Michael Hiley <hiley@earthlinginteractive.com>
2019-05-14 15:30:54 -0500

    add note about possible error

--- packages/api-lib/mike.sh
+++ packages/api-lib/mike.sh
@@ -6,4 +6,6 @@ export AWS_REGION=us-west-2
 export AWS_ACCESS_KEY_ID=omitted
 export AWS_SECRET_ACCESS_KEY=omitted
 
+# if you get an error like "Unable to connect to elasticsearch"
+# try increasing the ping requestTimeout in libs/es.js
 node mike.js
